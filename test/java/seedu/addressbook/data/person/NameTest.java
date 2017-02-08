0c84a709e6dfafdb5d2394865882ccc708b0f709
How Si Wei <howsiwei@protonmail.com>
2017-02-08 14:29:18 +0800

    Add unit test name_equal_returnsTrue

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -9,6 +9,13 @@ import seedu.addressbook.data.exception.IllegalValueException;
 
 public class NameTest {
 
+    @Test
+    public void name_equal_returnsTrue() throws IllegalValueException {
+        Name name0 = new Name("How Si Wei");
+        Name name1 = new Name("How Si Wei");
+        assertNameSimilar(name0, name1);
+    }
+
     private void assertNameSimilar(Name name0, Name name1) {
         assertTrue(name0.isSimilar(name1));
     }
